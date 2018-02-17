3ee379fcd47a199ad4e94714e3100881949a09a0
mabaranov <mail.baranov@gmail.com>
2018-02-17 22:08:51 +0300

    step 8

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -3,8 +3,10 @@ import even from '../games/check-even';
 import calc from '../games/calc';
 import gcd from '../games/gcd';
 import balance from '../games/balance';
+import progression from '../games/progression';
 
 even();
 calc();
 gcd();
 balance();
+progression();
---
+++ src/games/progression.js
@@ -0,0 +1,34 @@
+import play from '..';
+
+const minInt = 1;
+const maxInt = 100;
+const lengthSeq = 10;
+const maxStep = 10;
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * ((max - min) + 1)) + min;
+
+const rule = 'What number is missing in this progression?';
+
+const progression = () => {
+  let num = getRandomInt(minInt, maxInt);
+  const step = getRandomInt(1, maxStep);
+  const randomNum = getRandomInt(1, lengthSeq);
+  let question = '';
+  let answer;
+  let i = 1;
+
+  do {
+    if (randomNum === i) {
+      answer = String(num);
+      question += '.. ';
+    } else {
+      question += `${num} `;
+    }
+    i += 1;
+    num += step;
+  } while (i <= lengthSeq);
+
+  return { question, answer };
+};
+
+export default () => play(progression, rule);
